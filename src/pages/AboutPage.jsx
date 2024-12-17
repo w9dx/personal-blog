a00968189cab42156c044d038610f766e86b7a78
winters <dannywinter9911@outlook.com>
2024-12-17 15:07:21 +0700

    add detail about me

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,30 @@
+import myPicture from "../assets/ME.jpg";
 export default function AboutPage() {
-  return <h1>This is my About page!</h1>;
+  return (
+    <>
+      <h1>Hello</h1>
+      <div className="flex gap-3">
+        <div>
+          <img src={myPicture} alt="Andrew Picture" />
+        </div>
+        <div>
+          <h2>My name is Andrew Pham.</h2>
+          <p>
+            I am a software developer with a passion for creating beautiful and
+            functional websites. I have experience in both front-end and
+            back-end development, and I am always looking to learn new
+            technologies and improve my skills.
+          </p>
+          <p>
+            I'm currently looking for opportunity to dedicate as a fullstack
+            developer. If you interested in my profile, please feel free to
+            contact me.
+          </p>
+          <a href="https://www.linkedin.com/in/ngoc-pham-24613417b/">
+            Linkedin
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
