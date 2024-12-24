d078d83df3d0d3e56be013c9d7965327b63b522b
Daniels Winters <winters@192.168.88.53>
2024-12-24 14:21:35 +0700

    try add portfolio

--- src/pages/components/ShowCaseItem/index.jsx
+++ src/pages/components/ShowCaseItem/index.jsx
@@ -1,14 +1,23 @@
+import "./index.css";
 export default function ShowCaseItem({
   title,
   descriptions,
   imgSrc,
   references,
+  previewLink,
 }) {
   return (
     <div className="showcase-list__item">
       <div className="showcase-list__item__text">
         <h3 className="showcase-list__item__text__title">{title}</h3>
         <p className="showcase-list__item__text__description">{descriptions}</p>
+        <a
+          href={previewLink}
+          target="_blank"
+          className="showcase-list__item__text__preview"
+        >
+          Preview
+        </a>
       </div>
       <div className="showcase-list__item__image">
         <img src={imgSrc} alt="showcase" />
--- src/pages/showcases/ShowCaseListPage.jsx
+++ src/pages/showcases/ShowCaseListPage.jsx
@@ -1,16 +1,19 @@
 import ShowCaseItem from "../components/ShowCaseItem";
 import flexboxDemo from "../../assets/flex-box-desktop-design.jpg";
 import productList from "../../assets/product-list.jpg";
+import responsiveDesign from "../../assets/everyday-design.png";
+import "./showcase.css";
+
 export default function ShowCaseListPage() {
   return (
     <div className="showcase-list">
       <h1>Check out some of my works</h1>
-
       <ShowCaseItem
         title="Frontend Mentor Challenge: Flex Box Demo"
         descriptions={" "}
         references={`https://www.frontendmentor.io/challenges/bento-grid-RMydElrlOj`}
         imgSrc={flexboxDemo}
+        previewLink={"https://flex-box-demo.netlify.app/"}
       />
       <ShowCaseItem
         title={"Frontend Mentor Challenge: Product List"}
@@ -18,6 +21,17 @@ export default function ShowCaseListPage() {
           "https://www.frontendmentor.io/challenges/product-list-with-cart-5MmqLVAp_d"
         }
         imgSrc={productList}
+      />
+      <ShowCaseItem
+        title={"Linkedin Learning: Responsive Design"}
+        references={
+          "https://www.linkedin.com/learning/creating-a-responsive-web-design"
+        }
+        descriptions={
+          "Discover how to use responsive web design to make your site more readable and efficient—on any device."
+        }
+        imgSrc={responsiveDesign}
+        previewLink={"https://everyday-design.netlify.app"}
       />
     </div>
   );
