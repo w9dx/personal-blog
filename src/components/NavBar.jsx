f710de22ffa08f994d4160fe810e47750643eab0
Andy <110653750+w9dx@users.noreply.github.com>
2025-03-12 10:33:15 +0700

    play animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 // import logo from "../assets/simple_icon.webp";
 export default function NavBar() {
   return (
-    <nav className="mb-4 flex w-full items-center justify-center rounded-lg shadow-lg">
+    <nav className="mb-4 flex w-full items-center rounded-lg shadow-lg">
       <div className="logo h-12 w-12">
         {/* <img src={logo} alt="My Logo" /> */}
       </div>
-      <ul className="mx-auto w-3/4">
+      <ul className="mr-auto">
         <li>
           <Link to={"/"}>Home</Link>
         </li>
@@ -18,6 +18,8 @@ export default function NavBar() {
           <Link to={"/about/showcases"}>Show Cases</Link>
         </li> */}
       </ul>
+      <button>Login</button>
+
     </nav>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import myPicture from "../assets/ME.jpg";
 
 import Card from "../components/ui/Card";
 export default function HomePage() {
@@ -7,10 +6,14 @@ export default function HomePage() {
     <>
       <div className="flex w-full gap-4">
         <Card className="profile-card w-1/3">
+
         </Card>
+
         <div className="flex w-2/3 gap-4">
           <div className="flex h-full w-2/3 flex-col gap-4">
-            <div className="h-[300px] w-full rounded-lg bg-secondary p-4 shadow-lg"></div>
+            <div className="h-[300px] w-full rounded-lg bg-secondary p-4 shadow-lg">
+            <div className="excited">🥚</div>
+            </div>
             <div className="h-[400px] w-full rounded-lg bg-secondary p-4 shadow-lg"></div>
           </div>
           <div className="w-1/3">
